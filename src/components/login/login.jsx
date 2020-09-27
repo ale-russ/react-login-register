71ed8003eb7e75d26bf4c5b23e61dbc210d81d9e
ale-russ <agarrussom@gmail.com>
2020-09-27 23:59:22 +0300

    first commit

---
+++ src/components/login/login.jsx
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import loinImg from "../../login1.svg";
+
+export class Login extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="base-container" ref={this.props.containerRef}>
+                    <div className="header">Login</div>
+                    <div className="content">
+                        <div className="image">
+                            <img src={loinImg} />
+                        </div>
+                        <div className="form">
+                            <div className="form-group">
+                                <label htmlFor="username">Username</label>
+                                <input type="text" name="username" placeholder="username"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" placeholder="password"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <button className="btn" type="button">
+                            Login
+                        </button>
+                    </div>
+                </div>
+        )
+    }
+}
+
